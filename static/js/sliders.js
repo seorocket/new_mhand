c734d4933903892de71394bd06c008cd5eac57c0
Aleksandrov Ivan <101462143+Altorus@users.noreply.github.com>
2022-12-05 18:34:33 +0400

    05.12

---
+++ static/js/sliders.js
@@ -0,0 +1,12 @@
+new Swiper('.swiper-sale-calendar', {
+    navigation: {
+        prevEl: '.prev-sale-arrow',
+        nextEl: '.next-sale-arrow',
+    },
+})
+new Swiper('.shops-swiper', {
+    navigation: {
+        prevEl: '.prev-shops-calendar-arrow',
+        nextEl: '.next-shops-calendar-arrow',
+    },
+})
