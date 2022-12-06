972087daffc90e46f0845166647a6c96d238cbd6
Aleksandrov Ivan <101462143+Altorus@users.noreply.github.com>
2022-12-06 16:52:41 +0400

    06.12

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $(function () {
+        $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+            $(this)
+                .addClass('active').siblings().removeClass('active')
+                .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+        });
+    });
+})
--- static/js/sliders.js
+++ static/js/sliders.js
@@ -10,3 +10,64 @@ new Swiper('.shops-swiper', {
         nextEl: '.next-shops-calendar-arrow',
     },
 })
+new Swiper('.stock-catalog-swiper-women', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    observe:true,
+    observeParents:true,
+    setWrapperSize: true,
+    navigation: {
+        // prevEl: '.prev-shops-calendar-arrow',
+        nextEl: '.next-stock-slider-women-arrow',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar-women-stock',
+        draggable: true,
+    },
+})
+new Swiper('.stock-catalog-swiper-man', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    observe:true,
+    observeParents:true,
+    setWrapperSize: true,
+    navigation: {
+        // prevEl: '.prev-shops-calendar-arrow',
+        nextEl: '.next-stock-slider-man-arrow',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar-man-stock',
+        draggable: true,
+    },
+})
+new Swiper('.stock-catalog-swiper-child', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    observe:true,
+    observeParents:true,
+    setWrapperSize: true,
+    navigation: {
+        // prevEl: '.prev-shops-calendar-arrow',
+        nextEl: '.next-stock-slider-child-arrow',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar-child-stock',
+        draggable: true,
+    },
+})
+new Swiper('.brand-swiper', {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    observe: true,
+    observeParents: true,
+    setWrapperSize: true,
+    navigation: {
+        prevEl: '.prev-brand-arrow',
+        nextEl: '.next-brand-arrow',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar-brand',
+        draggable: true,
+    },
+})
+
