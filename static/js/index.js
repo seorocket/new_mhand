eff90df88cd9ca32c0a022123ff3839d188cdda7
Aleksandrov Ivan <101462143+Altorus@users.noreply.github.com>
2022-12-08 16:51:59 +0400

    08.12

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,44 @@
 $(document).ready(function () {
     $(function () {
         $('ul.tabs__caption').on('click', 'li:not(.active)', function () {
+            console.log($(this).index())
             $(this)
                 .addClass('active').siblings().removeClass('active')
                 .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
         });
     });
 
-    $(".our-collection-images-item").hover(function () {
-        const elemts = $('.our-collection-images-item')
-        for (let i=0; i < elemts.length; i++) {
+    $(".img-block-collection").hover(function () {
+        const elemts = $('.img-block-collection')
+        for (let i = 0; i < elemts.length; i++) {
             $(elemts[i]).css('width', '65px')
+
         }
         $(this).css('width', '140px')
         $($(this).next()).css('width', '85px')
         $($(this).prev()).css('width', '85px')
     })
 
-    $(".our-collection-images-item").mouseleave(function (e) {
-        $(this).css('width', '65px')
+    $(".img-block-collection").mouseleave(function (e) {
+        const elemts = $('.img-block-collection')
+        for (let i = 0; i < elemts.length; i++) {
+            $(elemts[i]).css('width', '65px')
+        }
+    })
+
+    $('.toggle-tabs').on('click', function () {
+        console.log($(this).next())
+        if ($(this).next().children('li.active').next().length != 0) {
+            $(this).next().children('li.active').next()
+                .addClass('active').siblings().removeClass('active')
+            $(this).next()
+                .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).next().children('li.active').index()).addClass('active');
+        } else {
+            $(this).next().children('li').eq(0)
+                .addClass('active').siblings().removeClass('active')
+            $(this).next()
+                .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).next().children('li.active').index()).addClass('active');
+        }
     })
 
 })
